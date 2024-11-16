01abf3dd7e9abbd43091a940eeebb45f41857fde
seethala e v <sheethalev@gmail.com>
2024-11-16 22:00:01 +0530

    initial commit

---
+++ src/Google/GoogleDemo.java
@@ -0,0 +1,14 @@
+package Google;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+	}
+
+}
