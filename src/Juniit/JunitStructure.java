eef7762b4f0c48149b7fbd5975a2ad80cc2709bf
seethala e v <sheethalev@gmail.com>
2024-11-17 22:17:28 +0530

    basic junit structure

---
+++ src/Juniit/JunitStructure.java
@@ -0,0 +1,29 @@
+package Juniit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JunitStructure 
+{
+	@Before
+	public void browseropen()
+	{
+		System.out.println("broswer open");	
+	}
+	@Test
+	public void test()
+	{
+		System.out.println("Test activityy");
+	}
+	@Test
+	public void test2()
+	{
+		System.out.println("Test2 activity");
+	}
+	@After
+	public void aftertest()
+	{
+		System.out.println("Browser close");
+	}
+}
