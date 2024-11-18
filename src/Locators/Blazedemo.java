556a4fdf3628cf6dd994d4f701d2a912f4906a9e
seethala e v <sheethalev@gmail.com>
2024-11-18 11:02:31 +0530

    Blazedemo register

---
+++ src/Locators/Blazedemo.java
@@ -0,0 +1,27 @@
+package Locators;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Blazedemo 
+{
+	ChromeDriver driver;
+	@org.junit.Before
+	 public void Before()
+		{
+			driver=new ChromeDriver();
+			driver.get("https://blazedemo.com/register");
+		}
+	@Test
+	public void test()
+	{
+		driver.findElement(By.name("name")).sendKeys("seethu",Keys.ENTER);
+		driver.findElement(By.name("company")).sendKeys("abc solutions",Keys.ENTER);
+		driver.findElement(By.id("email")).sendKeys("a@gmail.com",Keys.ENTER);
+		driver.findElement(By.id("password")).sendKeys("122aas",Keys.ENTER);
+		driver.findElement(By.id("password-confirm")).sendKeys("122aas",Keys.ENTER);
+	}
+
+}
