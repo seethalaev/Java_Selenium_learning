547bdbeca5882d0652cd98231b8ae1c9537d23bc
seethala e v <sheethalev@gmail.com>
2024-11-18 08:56:30 +0530

    Locategoogle

---
+++ src/Locators/GooleLocate.java
@@ -0,0 +1,24 @@
+package Locators;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GooleLocate 
+{
+	ChromeDriver driver;
+  @org.junit.Before
+  public void Before()
+  {
+	  driver=new ChromeDriver();
+	  driver.get("https://www.google.com/");
+  }
+   
+  @Test
+  public void test()
+  {
+	  driver.findElement(By.name("q")).sendKeys("selenium",Keys.ENTER);
+	//driver.findElement(By.name("btnk")).click();
+  }
+}
