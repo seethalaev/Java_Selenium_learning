d588e1057719b97c789d9ed39d282d0bb66f0784
seethala e v <sheethalev@gmail.com>
2024-11-19 22:47:43 +0530

    load reddif,using select class give values to dropdownfield of date,moth,year,count got

--- src/Dropdown/ReddifDropdown.java
+++ src/Dropdown/ReddifDropdown.java
@@ -1,4 +1,7 @@
+//load reddif,use select class and give values to dropdown of date,month,year
 package Dropdown;
+
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -20,16 +23,24 @@ public class ReddifDropdown
 	  @Test
 	  public void test()
 	  {
+		  
 		  WebElement dateelement = driver.findElement(By.xpath("//*[@id=\"tblcrtac\"]/tbody/tr[22]/td[3]/select[1]"));
 		  Select obj=new Select(dateelement);                    //constructor htrough value pass cheythath mansasilayilla
 		  obj.selectByValue("07");
+		  List<WebElement> li = obj.getOptions();                //obj enthukond choose cheythu enn areela
+		  System.out.println(li.size());
+		  
 		  
 		  WebElement monthelement = driver.findElement(By.xpath("//*[@id=\"tblcrtac\"]/tbody/tr[22]/td[3]/select[2]"));
 		  Select obj2=new Select(monthelement);
 		  obj2.selectByIndex(7);
+		  List<WebElement> li2 = obj2.getOptions();
+		  System.out.println(li2.size());
 		  
 		  WebElement yearelement = driver.findElement(By.xpath("//*[@id=\"tblcrtac\"]/tbody/tr[22]/td[3]/select[3]"));
 		  Select obj3=new Select(yearelement);
 		  obj3.selectByValue("2001");
+		  List<WebElement> li3 = obj3.getOptions();
+		  System.out.println(li3.size());
 	  }
 }
