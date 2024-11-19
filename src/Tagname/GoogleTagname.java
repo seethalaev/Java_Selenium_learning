86cef452a611c6e5d99c937a0d90e7298cbeb658
seethala e v <sheethalev@gmail.com>
2024-11-19 17:34:09 +0530

    images link checked using tagname

---
+++ src/Tagname/GoogleTagname.java
@@ -0,0 +1,27 @@
+package Tagname;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleTagname 
+	{
+	
+		  ChromeDriver driver;
+		  @Before
+		  public void before()
+		  {
+			  driver=new ChromeDriver();
+			  driver.get("https://www.google.com/");
+		  }
+		  @Test
+		  public void test()
+		  {
+			 List<WebElement> li = driver.findElements(By.tagName("a"));
+			 System.out.println(li.size());
+		  }
+	}
