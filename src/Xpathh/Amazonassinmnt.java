8a678412d99b20e0f73f0033e72db3c8e3ce79cf
seethala e v <sheethalev@gmail.com>
2024-11-19 09:11:48 +0530

    load amazon,parent to child locate mobiles

---
+++ src/Xpathh/Amazonassinmnt.java
@@ -0,0 +1,26 @@
+//load amazon,locate parent to child.//*[@id='nav-xshop']--parent
+package Xpathh;
+
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Amazonassinmnt 
+{
+  ChromeDriver driver;
+  @Before
+  public void before()
+  {
+	  driver=new ChromeDriver();
+	  driver.get("https://www.amazon.in/");
+  }
+  @org.junit.Test
+  public void Giftideas()
+  {
+	  driver.manage().window().maximize();
+	 //driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys("mobiles",Keys.ENTER);
+	 //driver.findElement(By.xpath("//span id[@id=\"nav-cart-count']")).click();
+	  driver.findElement(By.xpath("//*[@id='nav-xshop']/a[6]")).click();
+  } 
+}
