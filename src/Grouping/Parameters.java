4c7848020af24a9b73619134d956f75186a2c261
seethala e v <sheethalev@gmail.com>
2024-11-28 20:10:30 +0530

    parameter giving

--- src/Grouping/Parameters.java
+++ src/Grouping/Parameters.java
@@ -1,13 +1,5 @@
 package Grouping;
 
-import org.testng.annotations.Test;
+public class Parameters {
 
-public class Parameters 
-{
-	@org.testng.annotations.Parameters("a")
-	@Test
-	public static void main(String v)
-	{
-		System.out.println("Parameter values is:"+v);
-	}
 }
