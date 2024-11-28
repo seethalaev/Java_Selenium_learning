145bc4697b13c425866e43b1f8a3c567ec4fc42d
seethala e v <sheethalev@gmail.com>
2024-11-28 20:02:09 +0530

    fb open in edge,browser,chrome

--- src/Grouping/Crossbrowser.java
+++ src/Grouping/Crossbrowser.java
@@ -1,36 +1,5 @@
-
 package Grouping;
 
+public class Crossbrowser {
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
-
-public class Crossbrowser 
-{
-	WebDriver driver;
-	@Parameters("browser")
-	@BeforeTest
-	public void crossbr(String browser)
-	{ if(browser.equalsIgnoreCase("firefox"))
-	{
-		driver=new FirefoxDriver();
-	}
-	  
-	   else if(browser.equalsIgnoreCase("chrome"))
-		{
-			driver=new ChromeDriver();
-		}
-		else if(browser.equalsIgnoreCase("edge"))
-		{
-			driver=new EdgeDriver();
-		}
-		}
-		@org.testng.annotations.Test
-		public void fb()
-		{
-			driver.get("https:/www.facebook.com");
-		}}
+}
