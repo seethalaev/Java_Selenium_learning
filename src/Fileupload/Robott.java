08566a1d46e2dac9661450f71eacb815c614b251
seethala e v <sheethalev@gmail.com>
2024-11-29 22:15:53 +0530

    file upload using sendkey

--- src/Fileupload/Robott.java
+++ src/Fileupload/Robott.java
@@ -1,53 +1,5 @@
 package Fileupload;
 
-import java.awt.AWTException;
-import java.awt.Robot;
-import java.awt.Toolkit;
-import java.awt.datatransfer.StringSelection;
-import java.awt.event.KeyEvent;
+public class Robott {
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-
-public class Robott
-{
-
-	WebDriver driver;
-	@BeforeTest
-	public void beforemethod()
-	{
-		driver=new ChromeDriver();
-	}
-	@BeforeMethod
-	public void urlloading()
-	{
-		driver.get("https://www.ilovepdf.com/pdf_to_word");
-	}
-	@Test
-	public void test() throws AWTException
-	{
-		driver.findElement(By.xpath("//*[@id=\"pickfiles\"]")).click();
-		
-		fileupload("\"D:\\resumes\\Seethala..pdf\"");
-	}
-	public  void fileupload(String p) throws AWTException //variable
-	{
-		
-		StringSelection str=new StringSelection(p);  //select path
-		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(str, null);  //to copy to clipboard
-		Robot rbt=new Robot();		//to paste into syswind
-		rbt.delay(3000);
-		
-		rbt.keyPress(KeyEvent.VK_CONTROL);
-		rbt.keyPress(KeyEvent.VK_V);
-		rbt.keyRelease(KeyEvent.VK_V);
-		rbt.keyRelease(KeyEvent.VK_CONTROL);
-		rbt.keyPress(KeyEvent.VK_ENTER);
-		rbt.keyRelease(KeyEvent.VK_ENTER);
-		
-	}
 }
