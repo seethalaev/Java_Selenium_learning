d641173b55482f5443b57ddb0fc3dddafeab13ad
seethala e v <sheethalev@gmail.com>
2024-11-29 22:22:05 +0530

    load google page and verify title

--- src/Google/GoogleDemo.java
+++ src/Google/GoogleDemo.java
@@ -1,29 +1,5 @@
-//Load google page
-//Title verify
 package Google;
-
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GoogleDemo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		ChromeDriver driver=new ChromeDriver();
-		driver.get("https://www.google.com/");
-		
-		String actualtitle=driver.getTitle();
-		String expec="Google";
-		if(actualtitle.equalsIgnoreCase(expec))
-		{
-			System.out.println("fail");
-		}
-		else
-		{
-			System.out.println("pass");
-			
-		}
-		//driver.close();//close current window
-		//driver.quit();//entire browser
-	}
 }
