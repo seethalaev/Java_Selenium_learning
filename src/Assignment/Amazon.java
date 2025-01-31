244188fdcbec2e66cdd110f18d5fcdd3f3bb9bdb
seethala e v <sheethalev@gmail.com>
2025-01-31 17:47:46 +0530

    amazon->mobiles

--- src/Assignment/Amazon.java
+++ src/Assignment/Amazon.java
@@ -3,34 +3,41 @@ package Assignment;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Amazon {
 	ChromeDriver driver;
-	String url="https://www.amazon.in";
+	
 	@Before
 	public void setUp()
 	{
 		driver=new ChromeDriver();
-		driver.get(url);
+		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		
 	}
 	@Test
 	public void amazon()
 	{
-	WebElement search=driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
-	search.sendKeys("mobiles");
-	search.submit();
-	WebElement bestsellers=driver.findElement(By.xpath("//a[@href='/gp/bestsellers/?ref_=nav_cs_bestsellers']"));
-	bestsellers.click();
-	WebElement signin=driver.findElement(By.xpath("//div[@id='nav-tools']/a/div/span"));
-	signin.click();
-	driver.navigate().back();  //go back to previous page
-	WebElement cart=driver.findElement(By.xpath("//div[@id='nav-tools']/a[4]/div/span"));
+	WebElement search=driver.findElement(By.xpath("//*[@id=\"twotabsearchtextbox\"]"));
+	search.clear();
+	search.sendKeys("mobiles",Keys.ENTER);
+	
+	//WebElement bestsellers=driver.findElement(By.xpath("//a[@href='/gp/bestsellers/?ref_=nav_cs_bestsellers']"));
+	//bestsellers.click();
+	WebElement cart=driver.findElement(By.xpath("//*[@id=\"nav-cart-count\"]"));
 	cart.click();
+	//driver.navigate().back();
+	WebElement signin=driver.findElement(By.xpath("//*[@id=\"nav-link-accountList-nav-line-1\"]"));
+	signin.click();
+	WebElement mail = driver.findElement(By.xpath("//*[@id=\"ap_email\"]"));
+	mail.sendKeys("sheeth",Keys.ENTER);
+	driver.findElement(By.xpath("*[@id=\"continue\"")).click();
+	driver.navigate().back();  //go back to previous page
 	driver.navigate().back();
+	
 	WebElement menu=driver.findElement(By.xpath("//i[@class='hm-icon nav-sprite']"));
 	menu.click();
 		
